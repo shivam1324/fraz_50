77182c6492bded38855dc7a3ff610b867bbfeede
Shivam Garg <shivamgarg1324@gmail.com>
2022-04-26 17:01:51 +0530

    First Ques optimized

--- q1.cpp
+++ q1.cpp
@@ -19,3 +19,26 @@ public:
         return s;
     }
 };
+
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> s;
+        int size=nums.size();
+        int diff;
+        unordered_map<int,int>m;
+        for(int i=0;i<size;i++)
+        {
+            diff=target-nums[i];
+            if(m.find(diff)!=m.end() && m.find(diff)->second !=i)
+            {
+                s.push_back(i);
+                s.push_back(m.find(diff)->second);
+                return s;
+            }
+            m[nums[i]]=i;
+        }
+        return s;
+    }
+};
